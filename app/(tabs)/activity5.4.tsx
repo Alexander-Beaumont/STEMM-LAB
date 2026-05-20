147af59c464ca15f68aec3dbd06664bc4dc447ac
Alexander Beaumont <Alexander.t.beaumont@gmail.com>
2026-05-21 00:46:43 +1000

    additional screen with more intructions

--- app/(tabs)/activity5.4.tsx
+++ app/(tabs)/activity5.4.tsx
@@ -1,15 +1,12 @@
 
-import ImageDecorator from '../image.js';
 import Screen from '../screen.js';
   
 
 export default function Activity5() {
     let activity = new Screen();
     activity.setTitle("Human Performance Lab")
-    activity.setText("Diagram:")
+    activity.setText("Write-up (on paper):\n\nWhich movement was the hardest to keep the vibration low?\n\nRecord the results\n\nWere you right?\n\nAny surprises?")
     activity.setButtonLink('/activity5.5')
-    const imgRef = require("@/assets/images/diagram5.png")
-    activity.addDecorator(new ImageDecorator(imgRef))
 
     return (
         activity.getScreenCode()
