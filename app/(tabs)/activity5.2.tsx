09409bd19f774642c08811c899ac0a649e6abf5d
Alexander Beaumont <Alexander.t.beaumont@gmail.com>
2026-05-20 22:01:02 +1000

    Human Performance Lab - Instuctions and equipment screen

---
+++ app/(tabs)/activity5.2.tsx
@@ -0,0 +1,13 @@
+import Screen from '../screen.js';
+  
+
+export default function Activity5() {
+    let activity = new Screen();
+    activity.setTitle("Human Performance Lab")
+    activity.setText("Equipment List\n   - Mobile Phone with Stemm Lab App\n   - Open space to move safely.")
+    activity.setButtonLink('/activity5.3')
+
+    return (
+        activity.getScreenCode()
+    );
+}
