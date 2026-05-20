d5d9477a208d4f9aea713ab3007a5091a1dab0c4
Gemcutter <spud.hjc@gmail.com>
2026-05-20 23:02:05 +1000

    fix up activity 2

--- app/(tabs)/activity2.6.tsx
+++ app/(tabs)/activity2.6.tsx
@@ -1,13 +1,23 @@
 
-import Camera from '../camera.js';
 import Screen from '../screen.js';
+import { SoundMeter } from '../sound_decorator.js';
   
 
 export default function Activity1() {
     let activity = new Screen();
     activity.setTitle("Sound Pollution")
-    activity.setText("This page is just placeholder for until a sound sensor is programmed")
-
+    activity.addDecorator({
+    getCode() {
+      return (
+        <SoundMeter
+          onSoundChange={(data: { approxDb: number; soundLevel: string }) => {
+            console.log('Approx dB:', data.approxDb);
+            console.log('Sound Level:', data.soundLevel);
+          }}
+        />
+      );
+    },
+  });
     return (
         activity.getScreenCode()
     );
